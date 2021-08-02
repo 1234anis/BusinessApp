efd2c5a0ca38efa72c4df453a6e45e0219e5f173
mohd anis <anismohd86@gmail.com>
2021-08-02 11:34:35 +0530

    my firsr commit rectWebsite

---
+++ src/About.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import web from "../src/images/hero-img.png";
+import { NavLink } from "react-router-dom";
+
+
+const About = () => {
+  return (
+    <>
+      {/* <Commom
+        name="Welcome to About page"
+        imgsrc={web}
+        visit="/contact"
+        btname="Contact Now"
+      /> */}
+       <section id="header" className="d-flex align-items-center">
+        <div className="container-fluid ">
+          <div className="row">
+            <div className="col-10 mx-auto">
+              <div className="row">
+                <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column">
+                  <h1>
+                    Welcome to about page
+                    <strong className="brand-name"> MOHD ANIS </strong>
+                  </h1>
+                  <h2 className="my-3">
+                    We are the team of talented developer making websites
+                  </h2>
+                  <div className="mt-3">
+                    <NavLink to="/about" className="btn-get-started ">
+                      Contact Now
+                    </NavLink>
+                  </div>
+                </div>
+
+                <div className="col-lg-6 order-1 order-lg-2 header-img">
+                  <img
+                    src={web}
+                    className="img-fluid animated"
+                    alt="Commom img"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default About;
